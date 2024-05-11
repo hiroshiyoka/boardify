e72621e3585a167f20aeca62c893bef8afd8667f
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-05-11 15:03:17 +0700

    feat(sidebar): add local storage for sidebar state

--- app/(platform)/(dashboard)/_components/sidebar.tsx
+++ app/(platform)/(dashboard)/_components/sidebar.tsx
@@ -1,9 +1,16 @@
 "use client";
 
+import { useLocalStorage } from "usehooks-ts";
+
 interface SidebarProps {
   storageKey?: string;
 }
 
-export const Sidebar = ({ storageKey }: SidebarProps) => {
+export const Sidebar = ({ storageKey = "t-sidebar-state" }: SidebarProps) => {
+  const [expanded, setExpanded] = useLocalStorage<Record<string, any>>(
+    storageKey,
+    {}
+  );
+
   return <div>Sidebar!</div>;
 };
