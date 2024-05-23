a7f28d7ff6727f23ec1632ac87bfc65f4e5b4558
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-05-24 02:21:42 +0700

    feat(actions): add CreateBoard schema definition

--- actions/createBoard.ts
+++ actions/createBoard.ts
@@ -1,6 +1,12 @@
 "use server";
 
+import { z } from "zod";
+
 import { db } from "@/lib/db";
+
+const CreateBoard = z.object({
+  title: z.string(),
+});
 
 export async function create(formData: FormData) {
   const title = formData.get("title") as string;
