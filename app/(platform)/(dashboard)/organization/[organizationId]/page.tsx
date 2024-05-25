bf6d2d8fb52bc1563098333799988e72f7557453
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-05-26 02:24:18 +0700

    feat(organization): display board titles on the organization page

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -6,7 +6,7 @@ const OrganizationPage = async () => {
   const boards = await db.board.findMany();
 
   return (
-    <div>
+    <div className="flex flex-col space-y-4">
       <form action={create}>
         <input
           id="title"
@@ -15,8 +15,13 @@ const OrganizationPage = async () => {
           placeholder="Enter a board title"
           className="border-black border p-1"
         />
+        <Button type="submit">Submit</Button>
       </form>
-      <Button type="submit">Submit</Button>
+      <div className="space-y-2">
+        {boards.map((board) => (
+          <div key={board.id}>Board title: {board.title}</div>
+        ))}
+      </div>
     </div>
   );
 };
