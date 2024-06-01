63248027816b8c3a1e0be7afd11898352c87eb93
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-06-01 10:15:03 +0700

    feat(actions): add handler to action board

---
+++ actions/create-board/index.ts
@@ -0,0 +1,37 @@
+"use server";
+
+import { auth } from "@clerk/nextjs/server";
+import { revalidatePath } from "next/cache";
+
+import { db } from "@/lib/db";
+
+import { InputType, ReturnType } from "./types";
+
+const handler = async (data: InputType): Promise<ReturnType> => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return {
+      error: "Unauthorized",
+    };
+  }
+
+  const { title } = data;
+
+  let board;
+
+  try {
+    board = await db.board.create({
+      data: {
+        title,
+      },
+    });
+  } catch (error) {
+    return {
+      error: "Failed to create.",
+    };
+  }
+
+  revalidatePath(`/board/${board.id}`);
+  return { data: board };
+};
