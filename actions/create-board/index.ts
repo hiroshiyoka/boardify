ddd8e422b27e28b6a6a89d196238fb34e9eca963
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-06-01 10:17:09 +0700

    feat(actions): add createBoard function to create board action

--- actions/create-board/index.ts
+++ actions/create-board/index.ts
@@ -4,8 +4,10 @@ import { auth } from "@clerk/nextjs/server";
 import { revalidatePath } from "next/cache";
 
 import { db } from "@/lib/db";
+import { createSafeAction } from "@/lib/create-safe-action";
 
 import { InputType, ReturnType } from "./types";
+import { CreateBoard } from "./schema";
 
 const handler = async (data: InputType): Promise<ReturnType> => {
   const { userId } = auth();
@@ -35,3 +37,5 @@ const handler = async (data: InputType): Promise<ReturnType> => {
   revalidatePath(`/board/${board.id}`);
   return { data: board };
 };
+
+export const createBoard = createSafeAction(CreateBoard, handler);
