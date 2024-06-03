40b947e3cccff223f0aa61e698bd4bdd723f1e35
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-06-04 01:21:08 +0700

    feat(organization): add info component page

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/info.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import Image from "next/image";
+import { useOrganization } from "@clerk/nextjs";
+
+export const Info = () => {
+  const { organization, isLoaded } = useOrganization();
+
+  if (!isLoaded) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div className="flex items-center gap-x-4">
+      <div className="w-[60px] h-[60px] relative">
+        <Image
+          fill
+          src={organization?.imageUrl!}
+          alt="Organization"
+          className="rounded-md object-cover"
+        />
+      </div>
+    </div>
+  );
+};
