f8224707033929b6103e1b9b67c673a3cb188395
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-06-05 23:50:21 +0700

    feat(organization): add separator component to organization page

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,9 +1,12 @@
+import { Separator } from "@/components/ui/separator";
+
 import { Info } from "./_components/info";
 
 const OrganizationPage = async () => {
   return (
     <div className="w-full mb-20">
       <Info />
+      <Separator className="my-4" />
     </div>
   );
 };
