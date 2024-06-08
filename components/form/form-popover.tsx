7949553426dd98ef82cc98b78210828c8f42c81f
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-06-09 00:56:58 +0700

    feat(components): add Popover close component to form popover page

--- components/form/form-popover.tsx
+++ components/form/form-popover.tsx
@@ -1,9 +1,13 @@
 "use client";
+
+import { X } from "lucide-react";
 import {
   Popover,
+  PopoverClose,
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
+import { Button } from "@/components/ui/button";
 
 interface FormPopoverProps {
   children: React.ReactNode;
@@ -30,6 +34,14 @@ export const FormPopover = ({
         <div className="text-sm font-medium text-center text-neutral-600 pb-4">
           Create board
         </div>
+        <PopoverClose asChild>
+          <Button
+            className="h-auto w-auto p-2 absolute top-2 right-2 text-neutral-500"
+            variant="ghost"
+          >
+            <X className="h-4 w-4" />
+          </Button>
+        </PopoverClose>
       </PopoverContent>
     </Popover>
   );
