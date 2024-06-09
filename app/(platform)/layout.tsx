5d6c950eec59c00fa7525ba4f8528e9edc05652a
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-06-10 00:56:21 +0700

    feat(platform): add Toaster from sonner to platform layout

--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -1,10 +1,12 @@
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "sonner";
 
 const PlatformLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <ClerkProvider
       publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
     >
+      <Toaster />
       {children}
     </ClerkProvider>
   );
