9344091582563fb81fd50c2b935aaea474ca8cba
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-06-18 14:27:01 +0700

    feat(board): add board title form component to board navbar

---
+++ app/(platform)/(dashboard)/board/[boardId]/_components/board-title-form.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { Board } from "@prisma/client";
+
+import { Button } from "@/components/ui/button";
+
+interface BoardTitleFormProps {
+  data: Board;
+}
+
+export const BoardTitleForm = ({ data }: BoardTitleFormProps) => {
+  return (
+    <Button className="font-bold text-lg h-auto w-auto p-1 px-2">
+      {data.title}
+    </Button>
+  );
+};
