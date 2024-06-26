7a5a662ce3fa9d44be92ac379a205854525d0a91
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-06-27 02:03:48 +0800

    feat(board): import list form component to list container component

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
@@ -2,6 +2,8 @@
 
 import { ListWithCards } from "@/types";
 
+import { ListForm } from "./list-form";
+
 interface ListContainerProps {
   data: ListWithCards[];
   boardId: string;
@@ -10,6 +12,7 @@ interface ListContainerProps {
 export const ListContainer = ({ data, boardId }: ListContainerProps) => {
   return (
     <ol>
+      <ListForm />
       <div className="flex-shrink-0 w-1" />
     </ol>
   );
