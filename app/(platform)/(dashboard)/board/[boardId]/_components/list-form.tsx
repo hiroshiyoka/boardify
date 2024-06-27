716c4e0c1ebc7321eda710044306305654d49379
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-06-28 02:15:32 +0800

    feat(board): import some hooks to list form component

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-form.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-form.tsx
@@ -1,10 +1,16 @@
 "use client";
 
 import { Plus } from "lucide-react";
+import { useState, useRef, ElementRef } from "react";
 
 import { ListWrapper } from "./list-wrapper";
 
 export const ListForm = () => {
+  const formRef = useRef<ElementRef<"form">>(null);
+  const inputRef = useRef<ElementRef<"input">>(null);
+
+  const [isEditing, setIsEditing] = useState(false);
+
   return (
     <ListWrapper>
       <button className="w-full rounded-md bg-white/80 hover:bg-white/50 transition p-3 flex items-center font-medium text-sm">
