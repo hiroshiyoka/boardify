f995f6eceac84e9499d466e25f837c7d3578a155
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-06-29 00:26:43 +0800

    feat(board): add onKeyDown function to implement disable editing handler

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-form.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-form.tsx
@@ -22,6 +22,12 @@ export const ListForm = () => {
     setIsEditing(false);
   };
 
+  const onKeyDown = (e: KeyboardEvent) => {
+    if (e.key === "Escape") {
+      disableEditing();
+    }
+  };
+
   return (
     <ListWrapper>
       <button className="w-full rounded-md bg-white/80 hover:bg-white/50 transition p-3 flex items-center font-medium text-sm">
