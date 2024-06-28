e923caee46c22ae6e553c787f3c7c3844955281a
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-06-29 00:22:21 +0800

    feat(board): add handler for enable editing

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-form.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-form.tsx
@@ -11,6 +11,13 @@ export const ListForm = () => {
 
   const [isEditing, setIsEditing] = useState(false);
 
+  const enableEditing = () => {
+    setIsEditing(true);
+    setTimeout(() => {
+      inputRef.current?.focus();
+    });
+  };
+
   return (
     <ListWrapper>
       <button className="w-full rounded-md bg-white/80 hover:bg-white/50 transition p-3 flex items-center font-medium text-sm">
