c6bfc458300f99b60899107b752826fc0d8394aa
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-07-03 00:57:38 +0800

    feat(board): add order data state function

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useState } from "react";
+
 import { ListWithCards } from "@/types";
 
 import { ListForm } from "./list-form";
@@ -10,6 +12,8 @@ interface ListContainerProps {
 }
 
 export const ListContainer = ({ data, boardId }: ListContainerProps) => {
+  const [orderedData, setOrderedData] = useState(data);
+
   return (
     <ol>
       <ListForm />
