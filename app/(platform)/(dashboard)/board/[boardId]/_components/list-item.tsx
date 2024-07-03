0f60d6af49aa600f087b2e24fe4f82335d061068
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-07-04 00:02:13 +0800

    fix(board): add change to list item background color

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
@@ -10,7 +10,7 @@ interface ListItemProps {
 export const ListItem = ({ data, index }: ListItemProps) => {
   return (
     <li className="shrink-0 h-full w-[272px] select-none">
-      <div className="w-full rounded-md bg-[#1f1f2f4] shadow-md pb-2"></div>
+      <div className="w-full rounded-md bg-[#f1f2f4] shadow-md pb-2"></div>
     </li>
   );
 };
