b10a0700403902b291df92313a8fc071897ae1e6
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-07-07 04:50:20 +0800

    feat(board): add some hooks and event to list header

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
@@ -1,16 +1,40 @@
 "use client";
 
 import { List } from "@prisma/client";
+import { useState, useRef, ElementRef } from "react";
+import { useEventListener } from "usehooks-ts";
 
 interface ListHeaderProps {
   data: List;
 }
 
 export const ListHeader = ({ data }: ListHeaderProps) => {
+  const [title, setTitle] = useState(data.title);
+  const [isEditing, setIsEditing] = useState(false);
+
+  const formRef = useRef<ElementRef<"form">>(null);
+  const inputRef = useRef<ElementRef<"input">>(null);
+
+  const enableEditing = () => {
+    setIsEditing(true);
+    setTimeout(() => {
+      inputRef.current?.focus();
+      inputRef.current?.select();
+    });
+  };
+
+  const onKeydown = (e: KeyboardEvent) => {
+    if (e.key === "Escape") {
+      formRef.current?.requestSubmit();
+    }
+  };
+
+  useEventListener("keydown", onKeydown);
+
   return (
     <div className="pt-2 px-2 text-sm font-semibold flex justify-between items-start gap-x-2">
       <div className="w-full text-sm px-2.5 py-1 h-7 font-medium border-transparent">
-        {data.title}
+        {title}
       </div>
     </div>
   );
