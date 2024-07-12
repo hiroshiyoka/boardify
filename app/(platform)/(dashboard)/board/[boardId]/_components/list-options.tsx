0970d9110ce207c6a1b6b36cbbbc4d75fdb150d7
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-07-13 02:32:04 +0800

    feat(board): add styling to list options with popover component

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-options.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-options.tsx
@@ -1,6 +1,15 @@
 "use client";
 
 import { List } from "@prisma/client";
+import { MoreHorizontal, X } from "lucide-react";
+
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+  PopoverClose,
+} from "@/components/ui/popover";
+import { Button } from "@/components/ui/button";
 
 interface ListOptionsProps {
   data: List;
@@ -9,8 +18,25 @@ interface ListOptionsProps {
 
 export const ListOptions = ({ data, onAddCard }: ListOptionsProps) => {
   return (
-    <div>
-      <div>List Options</div>
-    </div>
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button className="h-auto w-auto p-2" variant="ghost">
+          <MoreHorizontal className="h-4 w-4" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="px-0 pt-3 pb-3" side="bottom" align="start">
+        <div className="text-sm font-medium text-center text-neutral-600 pb-4">
+          List actions
+        </div>
+        <PopoverClose asChild>
+          <Button
+            className="h-auto w-auto p-2 absolute top-2 right-2 text-neutral-600"
+            variant="ghost"
+          >
+            <X className="h-4 w-4" />
+          </Button>
+        </PopoverClose>
+      </PopoverContent>
+    </Popover>
   );
 };
