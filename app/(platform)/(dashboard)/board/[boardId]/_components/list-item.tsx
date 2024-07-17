a8c5da32046ccbe3fdf52645f16e416683748de0
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-07-18 01:08:35 +0800

    feat(board): add card-form component to list item

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
@@ -5,6 +5,7 @@ import { ElementRef, useRef, useState } from "react";
 import { ListWithCards } from "@/types";
 
 import { ListHeader } from "./list-header";
+import { CardForm } from "./card-form";
 
 interface ListItemProps {
   data: ListWithCards;
@@ -31,6 +32,13 @@ export const ListItem = ({ data, index }: ListItemProps) => {
     <li className="shrink-0 h-full w-[272px] select-none">
       <div className="w-full rounded-md bg-[#f1f2f4] shadow-md pb-2">
         <ListHeader onAddCard={enableEditing} data={data} />
+        <CardForm
+          listId={data.id}
+          ref={textareaRef}
+          isEditing={isEditing}
+          enableEditing={enableEditing}
+          disableEditing={disableEditing}
+        />
       </div>
     </li>
   );
