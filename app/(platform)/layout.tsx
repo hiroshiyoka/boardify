a2fa27598d1952a713875df71c635fe3afce17bf
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-08-18 21:48:48 +0800

    feat(platform): add query provider component to paltform layout

--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -2,15 +2,18 @@ import { Toaster } from "sonner";
 import { ClerkProvider } from "@clerk/nextjs";
 
 import { ModalProvider } from "@/components/providers/modal-provider";
+import { QueryProvider } from "@/components/providers/query-provider";
 
 const PlatformLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <ClerkProvider
       publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
     >
-      <Toaster />
-      <ModalProvider />
-      {children}
+      <QueryProvider>
+        <Toaster />
+        <ModalProvider />
+        {children}
+      </QueryProvider>
     </ClerkProvider>
   );
 };
