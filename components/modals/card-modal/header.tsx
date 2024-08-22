83cc8385b96172b81d09bba453557d2836310dd1
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-08-22 22:48:07 +0800

    feat(modals): add all needed package to card modal header

--- components/modals/card-modal/header.tsx
+++ components/modals/card-modal/header.tsx
@@ -1,7 +1,9 @@
 "use client";
 
-import { useState } from "react";
 import { Layout } from "lucide-react";
+import { useParams } from "next/navigation";
+import { ElementRef, useRef, useState } from "react";
+import { useQueryClient } from "@tanstack/react-query";
 
 import { CardWithList } from "@/types";
 import { Skeleton } from "@/components/ui/skeleton";
@@ -12,15 +14,30 @@ interface HeaderProps {
 }
 
 export const Header = ({ data }: HeaderProps) => {
+  const params = useParams();
+  const queryClient = useQueryClient();
+
+  const inputRef = useRef<ElementRef<"input">>(null);
+
   const [title, setTitle] = useState(data.title);
+
+  const onBlur = () => {
+    inputRef.current?.form?.requestSubmit();
+  };
+
+  const onSubmit = (formData: FormData) => {
+    console.log(formData.get("title"));
+  };
 
   return (
     <div className="flex items-start gap-x-3 mb-6 w-full">
       <Layout className="w-5 h-5 mt-1 text-neutral-700" />
       <div className="w-full">
-        <form>
+        <form action={onSubmit}>
           <FormInput
             id="title"
+            ref={inputRef}
+            onBlur={onBlur}
             defaultValue={title}
             className="font-semibold text-xl px-1 text-neutral-700 bg-transparent border-transparent relative -left-1.5 w-[95%] focus-visible:bg-white focus-visible:border-input mb-0.5 truncate"
           />
