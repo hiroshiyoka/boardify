23887e5a7da03968edf3044a3829b582eabb8192
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-09-08 13:36:38 +0800

    feat(providers): add pro modal component to modal provider

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from "react";
 
+import { ProModal } from "@/components/modals/pro-modal";
 import { CardModal } from "@/components/modals/card-modal";
 
 export const ModalProvider = () => {
@@ -18,6 +19,7 @@ export const ModalProvider = () => {
   return (
     <>
       <CardModal />
+      <ProModal />
     </>
   );
 };
