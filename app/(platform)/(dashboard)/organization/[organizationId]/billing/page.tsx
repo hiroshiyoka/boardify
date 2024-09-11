431a1b149f59899dc3c1431be66ffc0e056dc84f
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-09-11 13:58:33 +0800

    feat(organization): add subscription button component to billing page

--- app/(platform)/(dashboard)/organization/[organizationId]/billing/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/billing/page.tsx
@@ -1,6 +1,8 @@
 import { checkSubscription } from "@/lib/subscription";
 import { Separator } from "@/components/ui/separator";
 
+import { SubscriptionButton } from "./_components/subscription-button";
+
 import { Info } from "../_components/info";
 
 const BillingPage = async () => {
@@ -10,6 +12,7 @@ const BillingPage = async () => {
     <div className="w-full">
       <Info isPro={isPro} />
       <Separator className="my-2" />
+      <SubscriptionButton isPro={isPro} />
     </div>
   );
 };
